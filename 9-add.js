5f41d3a44bef9f8decfe7805349960d3af5322b0
OLUWATOBI <akindekorede@yahoo.com>
2025-11-13 23:48:26 +0100

    Add 9-add.js script for addition

---
+++ 9-add.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+function add(a, b) {
+  return a + b;
+}
+
+const args = process.argv.slice(2);
+const num1 = parseInt(args[0], 10);
+const num2 = parseInt(args[1], 10);
+
+if (isNaN(num1) || isNaN(num2)) {
+  console.log('NaN');
+} else {
+  console.log(add(num1, num2));
+}
